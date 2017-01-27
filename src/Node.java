29249505dff3ab199582f026f4e69c65422a7d5b
Fawaz-Khayat <Fawaz-Khayat@users.noreply.github.com>
2017-01-27 18:06:47 -0500

    Add files via upload

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,18 @@
 
-public class LinkedNode {
-
+public class Node {
+	private String value;
+	private Node next;
+	
+	// Constructor
+	public Node(String value){
+		this.value = value;
+		next = null;
+	}
+	public Node getNext(){
+		
+		return next;
+	}
+	public void setNext(Node nextNode){
+		next = nextNode;
+	}
 }
